3b6ea3cfae2783006774177db6e88387569c4eef
sandiphembram2021 <sandiphembram405@gmail.com>
2024-06-30 11:16:51 +0530

    hggg

--- src/speech_recognition/integration.py
+++ src/speech_recognition/integration.py
@@ -1,17 +1,15 @@
-# integration.py
-
 import speech_recognition as sr
 
 def recognize_speech(audio_file):
-    # Initialize recognizer
+   
     recognizer = sr.Recognizer()
 
-    # Load audio file
+   
     with sr.AudioFile(audio_file) as source:
         audio_data = recognizer.record(source)
 
     try:
-        # Use Google Web Speech API
+        
         recognized_text = recognizer.recognize_google(audio_data, language="sat-IN")  # Replace with Santali language code if available
         print("Recognized text:", recognized_text)
         return recognized_text
@@ -24,7 +22,7 @@ def recognize_speech(audio_file):
         print(f"Could not request results from Google Speech Recognition service; {e}")
         return None
 
-# Example usage
+
 if __name__ == "__main__":
     audio_file_path = "path/to/your/audio/file.wav"
     recognize_speech(audio_file_path)
